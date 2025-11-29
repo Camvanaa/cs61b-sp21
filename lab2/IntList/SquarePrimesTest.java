f77c0e57f5077c691d31f0090e5994195bc4467f
Camvanaa <3499880997@qq.com>
2025-11-30 00:49:10 +0800

    feat(lab2): complete debug exercises and setup

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -12,9 +12,9 @@ public class SquarePrimesTest {
      */
     @Test
     public void testSquarePrimesSimple() {
-        IntList lst = IntList.of(14, 15, 16, 17, 18);
+        IntList lst = IntList.of(14, 2, 16, 17, 10);
         boolean changed = IntListExercises.squarePrimes(lst);
-        assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertEquals("14 -> 4 -> 16 -> 289 -> 10", lst.toString());
         assertTrue(changed);
     }
 }
